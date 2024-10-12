878fcf9473bcad6e06374b9e00c80ae00b485334
ivanordenes <iordenes@usm.cl>
2024-10-11 23:32:06 -0300

    segunda version y ya completo en materia de lo que es django para empezar a trabajarlo, simplemente se conectaron todo lo que se debia y aparecia en los ppt

--- juegoweb/settings.py
+++ juegoweb/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "homepage","Record","Aboutus",
 ]
 
 MIDDLEWARE = [
--- juegoweb/urls.py
+++ juegoweb/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from homepage import views
+from Aboutus import views
+from Record import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",include("homepage.urls")),
+    path("",include("Record.urls"))
+    path("",include("Aboutus.urls")),
+    path("",views.index),
 ]
